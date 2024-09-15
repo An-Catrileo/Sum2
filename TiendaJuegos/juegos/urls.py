ad744d7e00ca9f4e6a626707109c74f17e21e70c
An-Catrileo <an.catrileo@duocuc.cl>
2024-09-14 22:51:54 -0300

    feat: :sparkles: sube cambios nuevos

---
+++ TiendaJuegos/juegos/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from.import views
+
+urlpatterns= [
+    path('', views.index, name='index'),
+    path('registro/', views.registro, name='registro'),
+    path('accion/', views.accion, name='accion'),
+    path('gary_mod/', views.gary_mod, name='gary_mod'),
+    path('gta/', views.gta, name='gta'),
+    path('kirby/', views.kirby, name='kirby'),
+    path('login/', views.login, name='login'),
+    path('minecraft/', views.minecraft, name='minecraft'),
+    path('mundo_abierto/', views.mundo_abierto, name='mundo_abierto'),
+    path('resident_evil/', views.resident_evil, name='resident_evil'),
+    path('subnautica/', views.subnautica, name='subnautica'),
+    path('supermario/', views.supermario, name='supermario'),
+    path('supervivencia/', views.supervivencia, name='supervivencia'),
+    path('suspenso/', views.suspenso, name='suspenso'),
+    path('terror/', views.terror, name='terror'),
+    path('the_evil_within/', views.the_evil_within_2, name='the_evil_within'),
+    path('the_witcher/', views.the_witcher, name='the_witcher'),
+    path('theforest/', views.theforest, name='theforest'),
+    path('todosLosJuegos/', views.todosLosJuegos, name='todosLosJuegos'),
+    path('tom_raider/', views.tom_raider, name='tomb_raider'),
+    path('zelda/', views.zelda, name='zelda'),
+]
---
+++ TiendaJuegos/juegos/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'juegos/index.html')
+
+def accion(request):
+    return render(request, 'juegos/accion.html')
+
+def gary_mod(request):
+    return render(request, 'juegos/gary_mod.html')
+
+def gta(request):
+    return render(request, 'juegos/gta.html')
+
+def kirby(request):
+    return render(request, 'juegos/kirby.html')
+
+def login(request):
+    return render(request, 'juegos/login.html')
+
+def minecraft(request):
+    return render(request, 'juegos/minecraft.html')
+
+def mundo_abierto(request):
+    return render(request, 'juegos/mundo_abierto.html')
+
+def registro(request):
+    return render(request, 'juegos/registro.html')
+
+def resident_evil(request):
+    return render(request, 'juegos/resident_evil.html')
+
+def subnautica(request):
+    return render(request, 'juegos/subnautica.html')
+
+def supermario(request):
+    return render(request, 'juegos/supermario.html')
+
+def supervivencia(request):
+    return render(request, 'juegos/supervivencia.html')
+
+def suspenso(request):
+    return render(request, 'juegos/suspenso.html')
+
+def terror(request):
+    return render(request, 'juegos/terror.html')
+
+def the_evil_within_2(request):
+    return render(request, 'juegos/the_evil_within_2.html')
+
+def the_witcher(request):
+    return render(request, 'juegos/the_witcher.html')
+
+def theforest(request):
+    return render(request, 'juegos/theforest.html')
+
+def todosLosJuegos(request):
+    return render(request, 'juegos/todosLosJuegos.html')
+
+def tom_raider(request):
+    return render(request, 'juegos/tom_raider.html')
+
+def zelda(request):
+    return render(request, 'juegos/zelda.html')
