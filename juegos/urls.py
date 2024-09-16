25ccc7c1788faf7fb9cab38b26ab10e41c8240e5
An-Catrileo <an.catrileo@duocuc.cl>
2024-09-16 18:10:38 -0300

    feat: :sparkles: edito urls de productos

--- juegos/urls.py
+++ juegos/urls.py
@@ -24,4 +24,9 @@ urlpatterns= [
     path('todosLosJuegos/', views.todosLosJuegos, name='todosLosJuegos'),
     path('tom_raider/', views.tom_raider, name='tom_raider'),
     path('zelda/', views.zelda, name='zelda'),
+    
+    path('productos/', views.listar_productos, name='listar_productos'),
+    path('productos/crear', views.crear_producto, name='crear_producto'),
+    path('productos/editar/<int:pk>/', views.editar_producto, name='listar_productos'),
+    path('productos/eliminar/<int:pk>/', views.eliminar_producto, name='eliminar_productos'),
 ]
